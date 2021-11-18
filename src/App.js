ea170b2819cb0f9085201b815c11367f47fc1a9b
guibrbs <200037994@aluno.unb.br>
2021-11-18 18:56:05 -0300

    Commit mudanças no projeto como todo!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
-
     const [movement, setMovement] = useState(false);
 
     useEffect(() => {
@@ -22,33 +21,33 @@ export default () => {
     }, []);
 
     return (
-        <div>
-            <div className={movement ? "content--movement" : "content"}>
-            {/*<div className={"content"}>*/}
-            </div>
-            <div className={movement ? "letters--movement" : "letters--group"}>
-                <h1 className={"letters--style"}>O</h1>
-                <h1 className={"letters--style"}>LÁ</h1>
+        <section>
+            <div className={"white--margin"}/>
+            <div className={movement ? "rectangle--movement" : "rectangle"}>
                 <p className={movement ? "letters--scroll--disable" : "letters--scroll"}>scroll</p>
                 <ArrowDownwardIcon className={movement ? "disable--arrow" : "link hang-on-hover"} style={{fontSize: 15}}></ArrowDownwardIcon>
-            </div>
-            <div className={movement ? "letters--about--movement" : "letters--about"}>
-                <p>Me chamo Guilherme Barbosa Ferreira</p>
-                <p className={"letters--white"}>e este é o meu portfólio, onde poderá </p>
-                <p className={"letters--white"}>encontrar alguns projetos meus!</p>
-            </div>
-            <div>
-                <a className={movement ? "link--netflix--movement" : "link--netflix"} href={"https://guibrbs.github.io/ProjetoNetflix/"}>
-                    <img src={"https://www.caviarcriativo.com/wp-content/uploads/2020/06/Significados-da-Marca-Netflix.gif"} alt={"Clone Netflix"} width={150}/>
-                </a>
-                <div className={movement ? "info--netflix--movement" : "info--netflix"}>
-                    <p className={"info--netflix--white"}>Clone Netflix</p>
-                    <p>React Js</p>
-                    <p>HTML</p>
-                    <p>CSS3</p>
+                <div className={movement ? "letters--about--movement" : "letters--about"}>
+                    <p>Me chamo Guilherme Barbosa Ferreira</p>
+                    <p className={"letters--white"}>e este é o meu portfólio, onde poderá </p>
+                    <p className={"letters--white"}>encontrar alguns projetos meus!</p>
+                </div>
+                <div className={movement ? "content--about--movement" : "content--about"}>
+                    <a href={"https://guibrbs.github.io/ProjetoNetflix/"}>
+                        <img src={"https://www.caviarcriativo.com/wp-content/uploads/2020/06/Significados-da-Marca-Netflix.gif"} alt={"Clone Netflix"} width={150}/>
+                    </a>
+                    <div className={"info--netflix"}>
+                        <p className={"info--netflix--white"}>Clone Netflix</p>
+                        <p>React Js</p>
+                        <p>HTML</p>
+                        <p>CSS3</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className={movement ? "letters--apresentation--movement" : "letters--apresentation"}>
+                <h1 className={"letters--style"}>O</h1>
+                <h1 className={"letters--style"}>LÁ</h1>
+            </div>
+        </section>
     )
 }
 
