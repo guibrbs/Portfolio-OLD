ccb6d71bfc300ff309c9e2b7829f1ced0c12d31d
guibrbs <200037994@aluno.unb.br>
2021-11-29 01:53:20 -0300

    Commit alteração no tamanho das fontes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,28 @@ import Apresentation from "./Apresentation";
 export default () => {
 
     const [movement, setMovement] = useState(false);
+    const projects = [
+        {
+            link: "https://guibrbs.github.io/ProjetoNetflix/",
+            img: "https://www.caviarcriativo.com/wp-content/uploads/2020/06/Significados-da-Marca-Netflix.gif",
+            name: "Clone Netflix",
+            info: [
+                "React Js",
+                "HTML",
+                "CSS3"
+            ]
+        },
+        {
+            link: "https://guibrbs.github.io/calculadora/",
+            img: "https://media.giphy.com/media/3o85xFdBA27p6TJWgM/giphy.gif",
+            name: "Calculator",
+            info: [
+                "React Js",
+                "HTML",
+                "CSS3"
+            ]
+        }
+    ]
 
     useEffect(() => {
         const scrollListener = () => {
@@ -26,7 +48,6 @@ export default () => {
     return (
         <section>
             <div className={"white--margin"}>
-
                 {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
                 <a href={"https://github.com/guibrbs"} target={"_blank"} rel={"noreferrer"} style={{color: "black"}}><GitHubIcon className={"icons"} style={{fontSize: 32}}/></a>
                 <a href={"mailto: mrguilhermeferreira@gmail.com"} target={"_blank"} rel={"noreferrer"} style={{color: "black"}}><EmailIcon className={"icons"} style={{fontSize: 32}}/></a>
@@ -43,32 +64,21 @@ export default () => {
                         <p className={"letters--white"}>e este é o meu portfólio, onde poderá </p>
                         <p className={"letters--white"}>encontrar alguns de meus projetos.</p>
                     </div>
-                    <div className={movement ? "content--about--movement" : "content--about"}>
-                        <a href={"https://guibrbs.github.io/ProjetoNetflix/"} target={"_blank"} rel={"noreferrer"}>
-                            <img
-                                src={"https://www.caviarcriativo.com/wp-content/uploads/2020/06/Significados-da-Marca-Netflix.gif"}
-                                alt={"Clone Netflix"} width={150}/>
-                        </a>
-                        <div className={"info"}>
-                            <p id={"white"}>Clone Netflix</p>
-                            <p>React Js</p>
-                            <p>HTML</p>
-                            <p>CSS3</p>
+                    {projects.map(project => (
+                        <div className={movement ? "content--about--movement" : "content--about"}>
+                            <a href={project.link} target={"_blank"} rel={"noreferrer"}>
+                                <img
+                                    src={project.img}
+                                    alt={project.name} width={150}/>
+                            </a>
+                            <div className={"info"}>
+                                <p id={"white"}>{project.name}</p>
+                                {project.info.map(info => (
+                                    <p>{info}</p>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                    <div className={movement ? "content--about--movement" : "content--about"}>
-                        <a href={"https://guibrbs.github.io/calculadora/"} target={"_blank"} rel={"noreferrer"}>
-                            <img
-                                src={"https://media.giphy.com/media/3o85xFdBA27p6TJWgM/giphy.gif"}
-                                alt={"Calculator"} width={150} height={90}/>
-                        </a>
-                        <div className={"info"}>
-                            <p id={"white"}>Calculator</p>
-                            <p>React Js</p>
-                            <p>HTML</p>
-                            <p>CSS3</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             <div className={movement ? "letters--apresentation--movement" : "letters--apresentation"}>
