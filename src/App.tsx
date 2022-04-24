266dac7bfedbc5244f4a280f409ca343cac46432
guibrbs <200037994@aluno.unb.br>
2022-04-24 19:01:58 -0300

    :rocket: Adding the page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <div className="container dark">
-      
+    <div className="container light">
+      <div className="wrapper">
+        <Home />
+      </div>
     </div>
   )
 }
