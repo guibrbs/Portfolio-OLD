95ab3a5f2524c99caf941a38aaba766cadf7d4fb
guibrbs <200037994@aluno.unb.br>
2022-04-24 19:03:45 -0300

    :rocket: Setting up and styling header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,14 @@ import './styles.css'
 
 export default function Header() {
     return(
-        <div>
-
-        </div>
+        <header className="header">
+            <a href="/" className="logo">GBF</a>
+            <div className="links-wrapper">
+                <a href="/" className="links active">Home</a>
+                <a href="/" className="links">Projetos</a>
+                <a href="/" className="links">Sobre</a>
+            </div>
+            <i>moon</i>
+        </header>
     )
 }
