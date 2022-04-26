56cf04b173a710c4f624a8345d0b5c5c9a731251
guibrbs <200037994@aluno.unb.br>
2022-04-26 19:08:17 -0300

    :repeat::rocket: Changing & adding elements projects section

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -3,13 +3,26 @@ import './styles.css'
 
 export default function Projects() {
     return(
-        <div className="container-projects">
-            <div className="projects">
-                
+        <div className="container-area">
+            <h1 className="container-title">Principais projetos</h1>
+            <div className="arrow">
+                <i className="fa-solid fa-chevron-down"></i>
             </div>
-            <div className="projects">
+            <div className="container-projects">
+                <div className="projects" id="one">
 
+                </div>
+                <div className="projects" id="two">
+
+                </div>
+                <div className="projects" id="three">
+
+                </div>
+                <div className="projects" id="four">
+
+                </div>
             </div>
+            <a href="/"><h2 className="explore">Explore mais projetos</h2></a>
         </div>
     )
 }
