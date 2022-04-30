92904243b0b0c77f9426bca3bf86dc1bcb1038c2
guibrbs <200037994@aluno.unb.br>
2022-04-30 13:55:39 -0300

    :cool: Formating with prettier

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,42 +1,52 @@
 import React, { useContext, useEffect, useState } from "react";
 import { DarkModeContext } from "../../DarkModeContext";
-import './styles.css'
+import "./styles.css";
 
 export default function Header() {
-    const [scroll, setScroll] = useState(false)
+  const [scroll, setScroll] = useState(false);
 
-    useEffect(() => {
-        const scrollListener = () => {
-            if (window.scrollY > 2){
-                setScroll(true)
-            }
-            else{
-                setScroll(false)
-            }
-        }
-        window.addEventListener('scroll', scrollListener)
-        return () => {
-            window.removeEventListener('scroll', scrollListener)
-        }
-    }, [])
-    const [href, setHref] = useState('')
-    useEffect(()=>{
-        const localhref = window.location.pathname
-        setHref(localhref)
-    })
-    const {isActive, setActiveState} = useContext(DarkModeContext)
-    return(
-        <header className={scroll ? "header sticky" : "header"}>
-            <a href="/" className="logo">GBF</a>
-            <div className="links-wrapper">
-                <a href="/" className={href === '/' ? "links active" : "links"}>Home</a>
-                <a href="projetos" className={href === '/projetos' ? "links active" : "links"}>Projetos</a>
-                <a href="/" className={href === '/sobre' ? "links active" : "links"}>Sobre</a>
-            </div>
-            <div className="icon" onClick={() => setActiveState(!isActive)}>
-                <i className="fa-solid fa-moon" ></i>
-                <i className="fa-solid fa-sun" ></i>
-            </div>
-        </header>
-    )
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 2) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+    window.addEventListener("scroll", scrollListener);
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+  const [href, setHref] = useState("");
+  useEffect(() => {
+    const localhref = window.location.pathname;
+    setHref(localhref);
+  });
+  const { isActive, setActiveState } = useContext(DarkModeContext);
+  return (
+    <header className={scroll ? "header sticky" : "header"}>
+      <a href="/" className="logo">
+        GBF
+      </a>
+      <div className="links-wrapper">
+        <a href="/" className={href === "/" ? "links active" : "links"}>
+          Home
+        </a>
+        <a
+          href="/projetos"
+          className={href === "/projetos" ? "links active" : "links"}
+        >
+          Projetos
+        </a>
+        <a href="/" className={href === "/sobre" ? "links active" : "links"}>
+          Sobre
+        </a>
+      </div>
+      <div className="icon" onClick={() => setActiveState(!isActive)}>
+        <i className="fa-solid fa-moon"></i>
+        <i className="fa-solid fa-sun"></i>
+      </div>
+    </header>
+  );
 }
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,88 +1,102 @@
 import React from "react";
-import './styles.css'
-import Metabase from '../../assets/images/metabase.png'
-import SignIn from '../../assets/images/sliding-signin.png'
-import CloneNetflix from '../../assets/images/clone-netflix.png'
-import PongGame from '../../assets/images/pong-game.png'
-import InteractiveRating from '../../assets/images/interactive-rating.png'
-import CovidTracker from '../../assets/images/covid-tracker.png'
+import "./styles.css";
+import Metabase from "../../assets/images/metabase.png";
+import SignIn from "../../assets/images/sliding-signin.png";
+import CloneNetflix from "../../assets/images/clone-netflix.png";
+import LandingPage from "../../assets/images/landing-page.png";
+import InteractiveRating from "../../assets/images/interactive-rating.png";
+import CovidTracker from "../../assets/images/covid-tracker.png";
 
 export default function Projects() {
-    type Projects_map = object[]
-    const projects_map: Projects_map = [
-        {
-            link: "/",
-            img: Metabase,
-            alt: 'image-metabase',
-            title: 'INDICAA',
-            description: "Feito exclusivamente para coordenadores e pessoas da gestão da Universidade de Brasília, com o intuito de auxiliar a administração das matérias ofertadas, indicando através de gráficos disponíveis no Metabase.",
-            id: 'one'
-        },
-        {
-            link: "/",
-            img: SignIn,
-            alt: "image-sign-in",
-            title: "Sign in | Sign up",
-            description: "Mini projeto pessoal com o objetivo de aprimorar as habilidades técnicas no qual envolve o desenvolvimento de formulários de sign in e sign up, alternando entre si com animações sem nenhum framework.",
-            id: 'two'
-        },
-        {
-            link: "/",
-            img: CloneNetflix,
-            alt: "Clone Netflix",
-            title: "Clone Netflix",
-            description: "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento de uma interface parecida com a da Netflix, utilizando imagens captadas através de uma API open source.",
-            id: 'three'
-        },
-        {
-            link: "/",
-            img: PongGame,
-            alt: "Pong Game",
-            title: "Pong Game",
-            description: "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento de um jogo bastante famoso. Foi desenvolvido com um intuito de ser Jogador x Inteligência artificial. Bom jogo!",
-            id: 'four'
-        },
-        {
-            link: "/",
-            img: InteractiveRating,
-            alt: "Interactive Rating Component",
-            title: "Interactive Rating Component",
-            description: "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento um componenete interativo de classificação. ",
-            id: 'five'
-        },
-        {
-            link: "/",
-            img: CovidTracker,
-            alt: "Covid Tracker",
-            title: "Covid Tracker",
-            description: "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento de um rastreador de casos de COVID-19 no Brasil e no mundo, através de uma API open source.",
-            id: 'six'
-        }
-    ]
-    return(
-        <div className="projects-area">
-            <h1 className="container-title">Principais projetos</h1>
-            <div className="arrow">
-                <i className="fa-solid fa-chevron-down"></i>
+  type Projects_map = object[];
+  const projects_map: Projects_map = [
+    {
+      link: "/projetos/INDICAA",
+      img: Metabase,
+      alt: "image-metabase",
+      title: "INDICAA",
+      description:
+        "Feito exclusivamente para coordenadores e pessoas da gestão da Universidade de Brasília, com o intuito de auxiliar a administração das matérias ofertadas, indicando \
+        através de gráficos disponíveis no Metabase.",
+      id: "one",
+    },
+    {
+      link: "/projetos/SignIn",
+      img: SignIn,
+      alt: "image-sign-in",
+      title: "Sign in | Sign up",
+      description:
+        "Mini projeto pessoal com o objetivo de aprimorar as habilidades técnicas no qual envolve o desenvolvimento de formulários de sign in e sign up, alternando entre si \
+        com animações sem nenhum framework.",
+      id: "two",
+    },
+    {
+        link: "/projetos/Landing-Page",
+        img: LandingPage,
+        alt: "Landing Page",
+        title: "Landing Page",
+        description:
+        "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento de uma Landing Page responsiva em qualquer dispositivo, além de incluir o modo noturno\
+        animado.",
+        id: "three",
+    },
+    {
+      link: "/projetos/Clone-Netflix",
+      img: CloneNetflix,
+      alt: "Clone Netflix",
+      title: "Clone Netflix",
+      description:
+        "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento de uma interface parecida com a da Netflix, utilizando imagens captadas \
+        através de uma API open source.",
+      id: "four",
+    },
+    {
+      link: "/projetos/Interactive-Rating",
+      img: InteractiveRating,
+      alt: "Interactive Rating Component",
+      title: "Interactive Rating Component",
+      description:
+        "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento um componenete interativo de classificação. ",
+      id: "five",
+    },
+    {
+      link: "/projetos/Covid-Tracker",
+      img: CovidTracker,
+      alt: "Covid Tracker",
+      title: "Covid Tracker",
+      description:
+        "Projeto pessoal com o objetivo de aprimorar as habilidades técnicas envolvendo o desenvolvimento de um rastreador de casos de COVID-19 no Brasil e no mundo,\
+        através de uma API open source.",
+      id: "six",
+    },
+  ];
+  return (
+    <div className="projects-area">
+      <h1 className="container-title">Principais projetos</h1>
+      <div className="arrow">
+        <i className="fa-solid fa-chevron-down"></i>
+      </div>
+      <div className="container-projects">
+        {projects_map.map((project: any) => {
+          return (
+            <div className="projects" id={project.id} key={project.id}>
+              <a href={project.link}>
+                <img src={project.img} alt={project.alt} className="imgs" />
+                <div className="content-project">
+                  <h2 className="description-project">
+                    <b>{project.title}</b>
+                  </h2>
+                  <p className="description-project">{project.description}</p>
+                  <p className="description-project">Saiba mais</p>
+                </div>
+              </a>
             </div>
-            <div className="container-projects">
-                {projects_map.map((project: any) => {
-                    return(
-                        <div className="projects" id={project.id} key={project.id}>
-                            <a href={project.link}>
-                                <img src={project.img} alt={project.alt} className="imgs"/>
-                                <div className="content-project">
-                                    <h2 className="description-project"><b>{project.title}</b></h2>
-                                    <p className="description-project">{project.description}
-                                    </p>
-                                    <p className="description-project">Saiba mais</p>
-                                </div>
-                            </a>
-                        </div>
-                    )
-                })}
-            </div>
-            <h2 className="explore"><a href="/projetos">Explore mais projetos</a></h2>
-        </div>
-    )
+          );
+        })}
+      </div>
+      <h2 className="explore">
+        <a href="/projetos">Explore mais projetos</a>
+      </h2>
+    </div>
+  );
 }
