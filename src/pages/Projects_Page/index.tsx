5ce21ebe3b3e7b7f6e82733ffd6907c37dfc086a
guibrbs <200037994@aluno.unb.br>
2022-04-29 21:32:23 -0300

    :cool: Prettier adjust

--- src/pages/Projects_Page/index.tsx
+++ src/pages/Projects_Page/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import './styles.css'
+import "./styles.css";
 import Header from "../../components/Header";
 import Projects from "../../components/Projects";
 
 export default function Projects_Page() {
-    return(
-        <>
-            <Header />
-            <div className="projects-page">
-                <Projects />
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className="projects-page">
+        <Projects />
+      </div>
+    </>
+  );
 }
