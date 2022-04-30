2f5dc723375b244316879767c5b71f72824353de
guibrbs <200037994@aluno.unb.br>
2022-04-30 18:01:56 -0300

    :rocket: Adding remaining projects pages

--- src/pages/Projects_Page/individual_projects.tsx
+++ src/pages/Projects_Page/individual_projects.tsx
@@ -7,6 +7,7 @@ import LandingPage from "../../assets/images/landing-page.png";
 import CloneNetflix from '../../assets/images/clone-netflix.png'
 import InteractiveRating from '../../assets/images/interactive-rating.png'
 import CovidTracker from '../../assets/images/covid-tracker.png'
+import Footer from "../../components/Footer";
 
 export default function IndividualProjects() {
   type Individual_projects = object[];
@@ -63,6 +64,57 @@ export default function IndividualProjects() {
       link: 'https://guibrbs.github.io/landing_page/',
       id: '3'
     },
+    {
+      title: "Clone Netflix",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
+      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
+      modi laboriosam, totam numquam aspernatur animi?",
+      tech: 
+        <>
+          <i className="devicon-react-original colored" title="React"></i>
+          <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
+          <i className="devicon-eslint-original colored" title="Eslint"></i>
+          <i className="devicon-materialui-plain colored" title="MaterialUI"></i>
+          <i className="devicon-css3-plain colored" title="CSS"></i>
+          <a href="https://github.com/guibrbs/ProjetoNetflix" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        </>,
+      img: CloneNetflix,
+      link: 'https://guibrbs.github.io/ProjetoNetflix/',
+      id: '4'
+    },
+    {
+      title: "Interactive Rating",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
+      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
+      modi laboriosam, totam numquam aspernatur animi?",
+      tech: 
+        <>
+          <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
+          <i className="devicon-css3-plain colored" title="CSS"></i>
+          <i className="devicon-html5-plain colored" title="HTML"></i>
+          <a href="https://github.com/guibrbs/Interacting_rating_component" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        </>,
+      img: InteractiveRating,
+      link: 'https://guibrbs.github.io/Interacting_rating_component/',
+      id: '5'
+    },
+    {
+      title: "Covid-19 Tracker",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
+      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
+      modi laboriosam, totam numquam aspernatur animi?",
+      tech: 
+        <>
+          <i className="devicon-react-original colored" title="React"></i>
+          <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
+          <i className="devicon-materialui-plain colored" title="MaterialUI"></i>
+          <i className="devicon-css3-plain colored" title="CSS"></i>
+          <a href="https://github.com/guibrbs/Covid-Tracker" target={"_blank"} rel={"noreferrer noopener"}><i className="devicon-github-original colored"></i></a>
+        </>,
+      img: CovidTracker,
+      link: 'https://guibrbs.github.io/Covid-Tracker/',
+      id: '6'
+    },
   ];
   const [url, setUrl] = useState('')
   useEffect(()=>{
@@ -112,6 +164,7 @@ export default function IndividualProjects() {
           );
         }
       })}
+      <Footer />
     </>
   );
 }
