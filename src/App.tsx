67d023573c251976c7a6df492e2d58aa4fe26848
guibrbs <200037994@aluno.unb.br>
2022-04-30 22:23:04 -0300

    :rocket: Adding page /sobre route path

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Projects_Page from './pages/Projects_Page'
 import { DarkModeContext } from './DarkModeContext'
 import IndividualProjects from './pages/Projects_Page/individual_projects'
+import About from './pages/About'
 
 function App() {
   const {isActive} = useContext(DarkModeContext)
@@ -21,6 +22,7 @@ function App() {
           <Route path='/projetos/Clone-Netflix' element={<IndividualProjects />}/>
           <Route path='/projetos/Interactive-Rating' element={<IndividualProjects />}/>
           <Route path='/projetos/Covid-Tracker' element={<IndividualProjects />}/>
+          <Route path='/sobre' element={<About />}/>
         </Routes>
       </div>
     </div>
