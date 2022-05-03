9604668b50237ff2b338bd6cc2b3882546d5e0c7
guibrbs <200037994@aluno.unb.br>
2022-05-03 17:52:55 -0300

    :rocket: Adding description for each project and changing the color

--- src/pages/Projects_Page/individual_projects.tsx
+++ src/pages/Projects_Page/individual_projects.tsx
@@ -14,9 +14,10 @@ export default function IndividualProjects() {
   const individual_projects: Individual_projects = [
     {
       title: "INDICAA",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
-      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
-      modi laboriosam, totam numquam aspernatur animi?",
+      description: "INDICAA é um projeto desenvolvido por alunos da matéria de Métodos de Desenvolvimento de Software da Universidade de Brasília - UnB com o objetivo \
+      de facilitar o acesso às informações apresentadas pelo site acadêmico SIGAA. É um projeto que visa a busca e a disponibilização \
+      visual de informações acadêmicas relevantes aos coordenadores da Universidade de Brasília - UnB, e também, possibilitar a pesquisa e a filtragem de dados por \
+      meio da interface do Metabase.",
       tech: 
         <>
           <i className="devicon-heroku-original colored" title="Heroku"></i> 
@@ -33,9 +34,9 @@ export default function IndividualProjects() {
     },
     {
       title: "Sign In | Sign Up",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
-      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
-      modi laboriosam, totam numquam aspernatur animi?",
+      description: "Sign In | Sign Up é um projeto de cunho pessoal com o intuito de realizar, somente com CSS, a animação entre páginas de cadastro e login. As ilustrações \
+      foram coletadas a partir do site UnDraw, que disponibiliza gratuitamente seus arquivos. O objetivo com o desenvolvimento dessa interface foi para aprimorar as habilidades técnicas \
+      referentes às tecnologias utilizadas, procurando seguir boas práticas de programação e construir um código limpo.",
       tech: 
         <>
           <i className="devicon-react-original colored" title="React"></i>
@@ -49,9 +50,9 @@ export default function IndividualProjects() {
     },
     {
       title: "Landing Page",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
-      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
-      modi laboriosam, totam numquam aspernatur animi?",
+      description: "Landing Page é um projeto de cunho pessoal, seguido através de um guia no youtube e tem o intuito de desenvolver uma página totalmente responsiva, com animações \
+      e um efeito de Dark e Light mode incrível. O objetivo com o desenvolvimento dessa página foi para ter conhecimento sobre responsividade conforme a elaboração do projeto, \
+      além de aprender a realizar a troca para o modo noturno, no qual o mesmo mecanismo de troca foi utilizado neste portfólio.",
       tech: 
         <>
           <i className="devicon-react-original colored" title="React"></i>
@@ -66,9 +67,9 @@ export default function IndividualProjects() {
     },
     {
       title: "Clone Netflix",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
-      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
-      modi laboriosam, totam numquam aspernatur animi?",
+      description: "Esse projeto foi o primeiro onde eu tive contato com ReactJs através de uma live realizada no canal Bonieky Lacerda, no qual envolve o desenvolvimento \
+      de uma aplicação do zero, clonando visualmente a interface da Netflix. As informações utilizadas como séries em destaque, filtro por categoria, banner de séries/filmes \
+      foram coletadas através da API TMDB, contudo vale ressaltar que não são inclusos os catálogos disponibilizados oficialmente pela Netflix.",
       tech: 
         <>
           <i className="devicon-react-original colored" title="React"></i>
@@ -84,9 +85,9 @@ export default function IndividualProjects() {
     },
     {
       title: "Interactive Rating",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
-      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
-      modi laboriosam, totam numquam aspernatur animi?",
+      description: "Esse projeto foi um desafio disponbilizado pela plataforma Frontend Mentor, cujo desafio é desenvolver esse componente de classificação interativo e tentar \
+      se aproximar o mais possível com as imagens disponbilizadas. Meu objetivo ao desenvolver foi me desafiar ao tentar implementar uma aplicação básica do zero, sem ajuda nenhuma \
+      e com alguns conhecimentos prévios das técnologias utilizadas.",
       tech: 
         <>
           <i className="devicon-javascript-plain colored" title="Javascript" style={{color: "#FFA701"}}></i>
@@ -100,9 +101,9 @@ export default function IndividualProjects() {
     },
     {
       title: "Covid-19 Tracker",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore delectus quis nulla quas dolorem fuga tempora numquam facere quod esse, \
-      voluptatum, libero laborum doloremque labore a dolor hic maiores, corrupti ratione enim quisquam atque officia iste porro! Iure fuga corporis illo distinctio architecto veniam,\
-      modi laboriosam, totam numquam aspernatur animi?",
+      description: "Com o estouro da pandemia de COVID-19, achei interessante a realização de gráficos que dispobilizase as informações de casos de COVID sobre os casos de óbito \
+      por todo o mundo, ou seja, as regiões mais influentes estão inclusas, e todos os dados foram coletados através de uma API (dados não estão mais sendo atualizados). \
+      O objetivo com o desenvolvimento desse projeto foi aprender a coletar dados de API e representar graficamente.",
       tech: 
         <>
           <i className="devicon-react-original colored" title="React"></i>
