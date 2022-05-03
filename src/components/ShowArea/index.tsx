8cb991aef9703118626f57dfd09d35b79c55f6a4
guibrbs <200037994@aluno.unb.br>
2022-05-03 17:54:43 -0300

    :rocket: Adding framer motion on show area

--- src/components/ShowArea/index.tsx
+++ src/components/ShowArea/index.tsx
@@ -1,34 +1,56 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import './styles.css'
 import AvatarLight from '../../assets/images/AvatarLight.png'
 import AvatarDark from '../../assets/images/AvatarDark.png'
 import { DarkModeContext } from "../../DarkModeContext";
+import { motion } from "framer-motion";
 
 export default function ShowArea(){
     const {isActive} = useContext(DarkModeContext)
+    const item = {
+        hidden: { y: 20, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1
+        }
+      }
+    const container = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2
+          }
+        }
+    }
     return(
         <div className="showArea">
             <div className="showArea-info">
-                <h2 className="hello">Olá, me chamo</h2>
-                <h1 className="nome">Guilherme Barbosa</h1>
-                <p className="pessoal-description">Sou estudante de <b><i>Engenharia de Software</i></b> pela <b><i>Universidade de Brasília.</i></b> Esse é o meu portfólio, 
-                    onde você consegue encontrar alguns de meus projetos como <b><i>Desenvolvedor Web. </i></b> 
-                    Se estiver procurando alguém <b><i>competente e focado</i></b>, hoje é seu dia de sorte, pois esse sou eu!
-                </p>
-                <div className="btn-area">
-                    <p className="btn">Entre em contato</p>
-                    <a href="https://github.com/guibrbs" target={"_blank"} rel={"noreferrer noopener"} className="social" title="GitHub">
-                        <i className="fa-brands fa-github"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/guibrbs/" className="social" target={"_blank"} rel={"noreferrer noopener"} title="Linkedin">
-                        <i className="fa-brands fa-linkedin"></i>
-                    </a>
-                </div>
+                <motion.div initial="hidden" animate="visible" variants={container}>
+                    <motion.h2 variants={item} className="hello">Olá, me chamo</motion.h2>
+                    <motion.h1 variants={item} className="nome">Guilherme Barbosa</motion.h1>
+                    <motion.p variants={item} className="pessoal-description">
+                        Sou estudante de <b><i>Engenharia de Software</i></b> pela <b><i>Universidade de Brasília.</i></b> Esse é o meu portfólio, 
+                        onde você consegue encontrar alguns de meus projetos como <b><i>Desenvolvedor Web. </i></b> 
+                        Se estiver procurando alguém <b><i>competente e focado</i></b>, hoje é seu dia de sorte, pois esse sou eu!
+                    </motion.p>
+                    <motion.div variants={item} className="btn-area">
+                        <p className="btn">Entre em contato</p>
+                        <a href="https://github.com/guibrbs" target={"_blank"} rel={"noreferrer noopener"} className="social" title="GitHub">
+                            <i className="fa-brands fa-github"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/guibrbs/" className="social" target={"_blank"} rel={"noreferrer noopener"} title="Linkedin">
+                            <i className="fa-brands fa-linkedin"></i>
+                        </a>
+                    </motion.div>
+                </motion.div>
             </div>
-            <div className={"img"}>
-                <img src={AvatarLight} id="img" className={isActive ? "img-style static imgOff" : "static img-style"}/> 
-                <img src={AvatarDark} id="img" className={isActive ? "img-style static" : "static img-style imgOff"}/>        
-            </div>
+            <motion.div initial="hidden" animate="visible" variants={container} className={"img"}>
+                <motion.img variants={item} src={AvatarLight} id="img" className={isActive ? "img-style static imgOff" : "static img-style"}/> 
+                <motion.img variants={item} src={AvatarDark} id="img" className={isActive ? "img-style static" : "static img-style imgOff"}/>        
+            </motion.div>
         </div>
     )
 }
