6861931d2b67c2dda8ae6c813ed634bba3dbffe9
guibrbs <200037994@aluno.unb.br>
2022-05-04 01:28:54 -0300

    :rocket: Adding mobile responsiviness on Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,8 +4,8 @@ import './styles.css'
 export default function Footer() {
     return(
         <footer className="footer-container">
-            <div>
-                <p>Guilherme Barbosa Ferreira</p>
+            <div id="footer-description">
+                <h3>Guilherme Barbosa Ferreira</h3>
                 <p>Técnico em Informática</p>
                 <p>Graduando em Engenharia de Software</p>
             </div>
@@ -16,9 +16,7 @@ export default function Footer() {
                     <a href="https://wa.me/5561991199619" target={'_blank'} rel={"noreferrer noopener"} className="wpp"><i className="fa-brands fa-whatsapp"></i></a>
                 </div>
                 <p>Email: mrguilhermeferreira@gmail.com</p>
-
             </div>
-
         </footer>
     )
 }
